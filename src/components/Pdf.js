bde59912426a7bf7017d4337b6e2cd780beb0050
David Crandall <davecrands@gmail.com>
2021-01-29 20:19:23 -0600

    Add page button styles

--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -6,28 +6,34 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 export default function Pdf() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const [pageZoom, setPageZoom] = useState(1.0);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
 
   return (
-    <div>
-      <Document
+    <div style={{outline: '1px solid red'}}>
+      <p className="text-center my-3">Page {pageNumber} of {numPages}</p>
+      <div className="flex mb-3">
+        {pageNumber > 1 &&
+            <button className="flex-auto bg-green-200 hover:bg-green-400 mx-1 p-1 rounded" onClick={() => setPageNumber(pageNumber - 1)}>
+              Previous page
+            </button>
+        }    
+        <button 
+          className="flex-auto bg-green-200 hover:bg-green-400 mx-1 p-1 rounded"          
+          onClick={() => setPageNumber(pageNumber + 1)}
+          >
+          Next page
+        </button>
+      </div>
+      <Document    
         file={algos}
         onLoadSuccess={onDocumentLoadSuccess}
-      >       
-            <Page pageNumber={pageNumber} />       
+        >       
+            <Page scale={pageZoom} pageNumber={pageNumber} />       
       </Document>
-      <p>Page {pageNumber} of {numPages}</p>
-      {pageNumber > 1 &&
-          <button onClick={() => setPageNumber(pageNumber - 1)}>
-            Previous page
-          </button>
-      }    
-      <button onClick={() => setPageNumber(pageNumber + 1)}>
-        Next page
-      </button>
     </div>
   );
 }
