63208d85bb99903debdcda10a1ee00ac3265177b
David Crandall <davecrands@gmail.com>
2021-03-06 16:46:05 -0600

    Import SelectPage, SetPageBtns

--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -4,6 +4,8 @@ import algos from '../docs/algos.pdf'
 import compilers from '../docs/Compilers.pdf'
 import { Document, Page, pdfjs } from "react-pdf";
 import ZoomBtns from "./ZoomBtns"
+import SetPageBtns from "./SetPageBtns"
+import SelectPage from "./SelectPage"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
@@ -14,49 +16,26 @@ export default function Pdf() {
   const [pageZoom, setPageZoom] = useState(1.0);
 
   function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);    
-    for (let i = 2; i <= numPages; i++) {      
-      let numObj = {value: i, label: i};    
+    setNumPages(numPages);
+    for (let i = 2; i <= numPages; i++) {
+      let numObj = { value: i, label: i };
       setpageNumberArr(state => [...state, numObj]);
-    }    
-  }
-
-  function onPageSelectChange(obj) {
-    setPageNumber(obj.value);
+    }
   }
 
   return (
-    <div style={{outline: '1px solid red', overflowY: 'hidden'}}>
-      <div style={{maxWidth:'250px', margin: "0 auto", display:"grid", gridTemplateColumns: "100px 150px", alignItems: "center"}}>
-      <span>Go to page: </span>
-      <Select        
-        options={pageNumberArr}
-        defaultValue={{ value: pageNumber, label: pageNumber }}
-        onChange={onPageSelectChange}
-        />
-      </div>
+    <div style={{ outline: '1px solid red', overflowY: 'hidden' }}>
+      <SelectPage pageNumber={pageNumber} pageNumberArr={pageNumberArr} setPageNumber={setPageNumber} />
       <p className="text-center my-3">
         Page {pageNumber} of {numPages}
       </p>
-      <div className="flex mb-3">
-        {pageNumber > 1 &&
-            <button className="flex-1 bg-green-200 hover:bg-green-400 mx-1 p-1 rounded" onClick={() => setPageNumber(pageNumber - 1)}>
-              Previous page
-            </button>
-        }    
-        <button 
-          className="flex-1 bg-green-200 hover:bg-green-400 mx-1 p-1 rounded"          
-          onClick={() => setPageNumber(pageNumber + 1)}
-          >
-          Next page
-        </button>
-      </div>
+      <SetPageBtns pageNumber={pageNumber} setPageNumber={setPageNumber} />
       <ZoomBtns pageZoom={pageZoom} setPageZoom={setPageZoom} />
-      <Document    
+      <Document
         file={compilers}
         onLoadSuccess={onDocumentLoadSuccess}
-        >       
-            <Page scale={pageZoom} pageNumber={pageNumber} />       
+      >
+        <Page scale={pageZoom} pageNumber={pageNumber} />
       </Document>
     </div>
   );
